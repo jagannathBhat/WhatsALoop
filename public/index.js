c4ec506451596feef04a8c22e4f286a482881cbc
Jagannath Bhat <jagannathbhatjb@gmail.com>
2019-05-24 15:27:00 +0530

    add slide0

--- public/index.js
+++ public/index.js
@@ -8,13 +8,22 @@
 	};
 
 	let nextSlide = () => {
-		document.getElementById("slide" + ++currentSlide).classList.add("slide--done");
+		document.getElementById("slide" + currentSlide++).classList.add("slide--done");
 		setTimeout(() => {
 			document.body.style.transform = "translateY(-" + (currentSlide * 100) + "%)";
 		}, 300);
 	};
 
+	let slide0action0 = () => {
+		document.getElementById("title__p1").classList.add("-close");
+		document.getElementById("title__p3").classList.add("-close");
+		setTimeout(() => {
+			document.getElementById("title__p2").classList.add("-open");
+		}, 300);
+	};
+
 	let actions = [
+		slide0action0,
 		nextSlide
 	];
 
